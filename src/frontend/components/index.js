c2f3588e5a60c859c6aaf6c30199a2406f698cd4
Clark Feusier <cfeusier@gmail.com>
2015-12-06 15:44:18 -0800

    Remove react/redux setup

--- src/frontend/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Application } from './application';
-export { default as Nav } from './nav';
-export { default as Home } from './home';
-export { default as About } from './about';
